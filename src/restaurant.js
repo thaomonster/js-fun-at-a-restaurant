3f842a7255c92fef5f697767b21f633be402333b
Thao Ma <MaThao@gmail.com>
2020-10-08 10:58:03 -0600

    Finish createRestaurant test block

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,17 @@
-
+function createRestaurant(name) {
+  var restaurantName = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  };
+  return restaurantName;
+};
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem
 }

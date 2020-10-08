ac993abf66fb9a93b39b471c979434071227f3c8
Thao Ma <MaThao@gmail.com>
2020-10-08 12:39:27 -0600

    Finish addMenuItem test block

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,8 +10,18 @@ function createRestaurant(name) {
   return restaurantName;
 };
 
+function addMenuItem(name, food) {
+  if (food.type === 'breakfast' && !name.menus.breakfast.includes(food) ) {
+    name.menus.breakfast.push(food)
+  } else if (food.type === 'lunch' && !name.menus.breakfast.includes(food)) {
+    name.menus.lunch.push(food)
+  } else if (food.type === 'dinner' && !name.menus.breakfast.includes(food)) {
+    name.menus.dinner.push(food)
+  }
+};
+
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
